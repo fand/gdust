520bf278142aa50d58889901cb84b4c14f38d974
fand <s0811489@coins.tsukuba.ac.jp>
2013-11-30 16:39:35 +0900

    fixed integrator sample generation

--- include/integrator.hpp
+++ include/integrator.hpp
@@ -13,13 +13,6 @@ public:
     float distance( TimeSeries &ts1, TimeSeries &ts2, int n );
     
 private:
-    float *in, *out, *sum;
-    float *in_GPU, *out_GPU, *param_GPU, *sum_GPU;
     curandGenerator_t *gen;
-
-    float *sum1, *sum2, *sum3;
-    float *out1_GPU, *out2_GPU, *out3_GPU;
-    float *sum1_GPU, *sum2_GPU, *sum3_GPU;
-    float *answer_GPU;
 };
 
