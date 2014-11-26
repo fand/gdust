0d4fd3ba6baa3e5cc51815cb30292b74f3fbe84d
fand <s0811489@coins.tsukuba.ac.jp>
2014-11-26 11:32:45 +0900

    use exec

--- script/temperture.js
+++ script/temperture.js
@@ -5,36 +5,36 @@
 
 var fs = require('fs');
 var sysPath = require('path');
-var execSync = require('child_process').execSync;
+var exec = require('child_process').exec;
 var expCmd = process.argv[2];
 
 var i = 0;
 var watchCmd = 'nvidia-smi --query-gpu="temperature.gpu" --format="csv" -i 1';
 var watch = function (cb) {
-  var temperature = (execSync(watchCmd) + "").match(/\d+/)[0];
-  if (temperature > 55) {
-    setTimeout(function () {
-      watch(cb);
-    }, 30000);
-  }
-  else {
-    // console.log('############################### temperature: ', temperature);
-    cb();
-  }
+  exec(watchCmd, function (err, stdout, stderr) {
+    if (err) { throw err; }
+    var temperature = (stdout + "").match(/\d+/)[0];
+    if (temperature > 55) {
+      setTimeout(function () {
+        watch(cb);
+      }, 30000);
+    }
+    else {
+      // console.log('############################### temperature: ', temperature);
+      cb();
+    }
+  });
 };
 
 var exp = function () {
-  try {
-    var result = execSync(expCmd + ' --target ' + i);
-    // console.log('result: ', result + "");
-    if (result == -1) { return; }
+  var cmd = expCmd + ' --target ' + i;
+  exec(expCmd + ' --target ' + i, function (err, stdout, stderr) {
+    if (err) { throw err; }
+    console.log(stdout);
     watch(function () {
       exp(++i);
     });
-
-  } catch(e) {
-    // console.log('end');
-  }
+  });
 };
 
 
